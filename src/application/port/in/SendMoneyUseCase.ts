f7f5d214f599d2ff7ad8ff362c94b48e5e17f9f3
tomoya <alligatorfree12@gmail.com>
2025-10-16 17:55:46 +0900

    コメントを追加

--- src/application/port/in/SendMoneyUseCase.ts
+++ src/application/port/in/SendMoneyUseCase.ts
@@ -1,17 +1,21 @@
-import { SendMoneyCommand } from './SendMoneyCommand';
+import {SendMoneyCommand} from './SendMoneyCommand';
 
 /**
  * 送金ユースケースのインターフェース（入力ポート）
  * ドメインサービスが実装する
  */
+
+// 「私に送金をしてほしいなら、SendMoneyCommand という形式でデータを渡して、sendMoney メソッドを呼んでください。
+// そうすれば、外側からの要求を私が処理しますよ」と言っているようなもの。
+// つまり、SendMoneyUseCaseインターフェースは、アプリケーションコアとアダプター層の境界線
 export interface SendMoneyUseCase {
-  /**
-   * 送金を実行
-   * 
-   * @param command 送金コマンド
-   * @returns 送金が成功したかどうか
-   */
-  sendMoney(command: SendMoneyCommand): Promise<boolean>;
+    /**
+     * 送金を実行
+     *
+     * @param command 送金コマンド
+     * @returns 送金が成功したかどうか
+     */
+    sendMoney(command: SendMoneyCommand): Promise<boolean>;
 }
 
 /**
